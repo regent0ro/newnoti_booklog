54821d177ef3bf38960a5eee6c6abd777aa43d97
y3-kang <26410322+regent0ro@users.noreply.github.com>
2023-05-11 00:57:23 +0900

    Add to new book list for selected month

--- newnoti_booklog.py
+++ newnoti_booklog.py
@@ -10,11 +10,13 @@ DEBUG_MODE = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--id', help='Booklog user id',type=str,required=True)
+parser.add_argument('--year', help='Year of registration',type=int,required=True)
 parser.add_argument('--month', help='Month of registration',type=int,required=True)
 
 args = parser.parse_args()
 user_id = args.id
 month = args.month
+year = args.year
 
 # リストを取得(とりあえず100件に制限)　→　sort順を確認する必要がある
 api_res=requests.get(f"https://api.booklog.jp/v2/json/{user_id}?count=100")
@@ -22,20 +24,35 @@ json_res=json.loads(api_res.text)
 books=json_res['books']
 
 if DEBUG_MODE:
-    print(json_res)
+    print(books)
 
 #本の情報を取得
 for book in books:
     html = requests.get(book['url'])
     soup = BeautifulSoup(html.content, "html.parser")
 
-    register_date = register_date=soup.find(class_='read-day-status-area').find('span').text
+    register_date = soup.find(class_='read-day-status-area').find('span').text
     
     if DEBUG_MODE:
         print(register_date)
 
-    # TODO 月でフィルタリングしてOKなら　著者、表紙を追加取得して配列に格納する
-    #amazon_link =soup.find(class_='itemInfoElm').find('a').get('href')
-    
+    register_date = datetime.datetime.strptime(register_date, '%Y年%m月%d日')
+
+    #入力した月に登録した本のみ登録
+    new_book_list = []
+    if register_date.year == year and register_date.month == month:
+
+        #著者は複数人いる場合がある
+        author_html = soup.find(class_='item-info-author').find_all('a')
+
+        new_book = {
+            'title' : book['title'],
+            'author' : ','.join((x.string for x in author_html)),
+            'book_cover' : book['image']
+        }
+        new_book_list.append(new_book)
+
+if DEBUG_MODE:
+    print(new_book_list)
 
 # TODO markdownのテーブル型式でリストを作成する。長いタイトル、著者名は...で表示する
